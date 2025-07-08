5f4ffaaac092c2beaac7c4723e7b49f5e83b0cf9
Dhruv-Bhaskar <dhruvkumarbhaskar599@outlook.com>
2025-07-08 14:40:13 +0530

    some minor changes

--- app.js
+++ app.js
@@ -28,7 +28,8 @@ app.use('/user',userRouter)
         //    message: 'Server error'
         // })
 
-app.listen(5000,()=>{
-    console.log('Server is running on port 5000');
-    
-})
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
